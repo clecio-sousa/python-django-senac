9626eb66d4bd0388ba6b4bc153661d07681d3ccf
clecio1201 <clecio1201@hotmail.com>
2020-02-07 22:30:15 -0300

    revisao de python

---
+++ lista_exercicios_01/questao_01.py
@@ -0,0 +1,17 @@
+valor_casa = float(input("Digite o valor da casa: "))
+valor_salario = float(input("Digite o salario: "))
+qtde_meses = int(input("Em quantos anos quer quitar a casa: "))
+
+sal_reduzido = (valor_salario*0.3)
+valor_prestacao = valor_casa/qtde_meses
+
+print("||CALCULO DA NEGOCIAÇÃ||")
+
+if(valor_prestacao > sal_reduzido):
+    print(f"VALOR DA PRESTAÇÃO - R${valor_prestacao:.2f}||| 30% DO SALARIO- R${sal_reduzido:.2f}")
+    print("EMPRESTIMO NÃO APROVADO: VALOR DA PRESTAÇÃO É SUPERIOR A 30% DO SALARIO")
+
+else:
+    print(f"VALOR DA PRESTAÇÃO - R${valor_prestacao:.2f}||| 30% DO SALARIO- R${sal_reduzido:.2f}")
+    print("EMPRESTIMO APROVADO")
+
