cc0489240699a6d7f0d6266cba4a3bfa523dcf8c
Clecio Sousa <clecio1201@hotmail.com>
2020-03-15 09:53:02 -0300

    Update cliente.py
    criacao do metodo resumo, no qual vai imprimir os dados do cliente

--- banco/cliente.py
+++ banco/cliente.py
@@ -1,4 +1,17 @@
 class Cliente:
-    def __init__(self, nome, telefone):
+    def __init__(self, nome, telefone, agencia, numero):
         self.nome = nome
         self.telefone = telefone
+        self.agencia = agencia
+        self.numero = numero
+
+
+    def resumo(self):
+
+        print(f"Nome: {self.nome}"
+              f"\nTelefone: {self.telefone}"
+              f"\nAgencia: {self.agencia}"
+              f"\nNumero da conta: {self.numero}")
+
+        
+
