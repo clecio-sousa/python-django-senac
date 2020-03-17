4baf6bb30b95d4d3e4fbdd8d9571bfccefcf0ac4
clecio1201 <you@example.com>
2020-03-17 02:01:57 -0300

    criacao da classe hospital e seus metodos

---
+++ projeto-corona-virus/hospital.py
@@ -0,0 +1,17 @@
+class Hospital:
+    def __init__(self):
+
+        self.lista_pacientes = []
+        self.lista_pacientes_infectados = []
+
+
+
+    def cadastrar_pacientes(self):
+
+        nome = input("Digite o nome do paciente: ")
+        idade = int(input("Digite a idade do paciente: "))
+        sexo = input("Digite o sexo do paciente: F OU M")
+
+
+    def listar_pacientes_infectados(self):
+        pass
