1607139f2f4cef3c7128bd49c98f944cc3b008ae
clecio1201 <you@example.com>
2020-03-17 02:03:06 -0300

    testando o codigo

---
+++ projeto-corona-virus/teste.py
@@ -0,0 +1,21 @@
+from sintomas import Paciente
+from hospital import Hospital
+
+paci = Hospital()
+opcao = 0
+while (True):
+
+    print("\n||||| BEM VINDO AO HOSPITAL PYTHON |||||"
+          "\n\n\t\t1 - CADASTRAR PACIENTE"
+          "\n\t\t2 - SAIR")
+
+    opcao = int(input("DIGITE UMA OPCAO :"))
+
+    if opcao == 2:
+        break
+
+    else:
+        if (opcao == 1 ):
+            paci.
+
+
