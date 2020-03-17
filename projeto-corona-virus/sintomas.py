5cfb1cf404eb435371b7a5e4ecbe9f2ca0fa33dc
clecio1201 <you@example.com>
2020-03-17 02:02:27 -0300

    criacao da classe sintomas

---
+++ projeto-corona-virus/sintomas.py
@@ -0,0 +1,17 @@
+class Paciente:
+    def __init__(self):
+
+         #SINTOMAS
+        self.febre_alta = input("Febre: ")
+        self.tosse_seca = input("Tosse seca: ")
+        self.coriza = input("Coriza: ")
+        self.dificuldade_respirar= input("Dificuldade para respirar :")
+        self.dores_musculares = input("Dores musculares: ")
+        self.diarreia = input("Diarreis: ")
+        self.dor_garganta = input("Dor de garganta: ")
+
+
+
+
+
+
