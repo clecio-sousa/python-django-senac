023732a21b8fe8793c87fe9cd6086e6b03eb0c63
clecio1201 <you@example.com>
2020-03-18 07:23:16 -0300

    atualizando  classe hospital

--- projeto-corona-virus/hospital.py
+++ projeto-corona-virus/hospital.py
@@ -4,14 +4,11 @@ class Hospital:
         self.lista_pacientes = []
         self.lista_pacientes_infectados = []
 
-
-
     def cadastrar_pacientes(self):
-
         nome = input("Digite o nome do paciente: ")
         idade = int(input("Digite a idade do paciente: "))
         sexo = input("Digite o sexo do paciente: F OU M")
 
-
     def listar_pacientes_infectados(self):
         pass
+
