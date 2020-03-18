1c1da0a3b891877f3e20f2bfad1098280eac6c1e
clecio1201 <you@example.com>
2020-03-18 07:22:32 -0300

    listando sintomas

--- projeto-corona-virus/sintomas.py
+++ projeto-corona-virus/sintomas.py
@@ -1,17 +1,28 @@
 class Paciente:
+
     def __init__(self):
 
-         #SINTOMAS
-        self.febre_alta = input("Febre: ")
+        #SINTOMAS
+
+
+    def listar_sintomas_gripe(self):
+        print("_" *30)
+        print("\nQUESTIONARIO P/ DIAGNOSTICO")
+        print("_" * 30)
+        self.febre_alta = input("Febre:")
         self.tosse_seca = input("Tosse seca: ")
         self.coriza = input("Coriza: ")
-        self.dificuldade_respirar= input("Dificuldade para respirar :")
+        self.dificuldade_respirar = input("Dificuldade para respirar :")
         self.dores_musculares = input("Dores musculares: ")
-        self.diarreia = input("Diarreis: ")
+        self.diarreia = input("Diarreia: ")
         self.dor_garganta = input("Dor de garganta: ")
+        self.dor_cabeca = input("Dor de cabeça: ")
 
 
-
-
-
-
+        """print("O paciente apresenta:"
+              f"\n\nFebre alta: {self.febre_alta}"
+              f"\nTosse seca: {self.tosse_seca}"
+              f"\nDificuldade para respirar: {self.dificuldade_respirar}"
+              f"\nDores musculares: {self.dores_musculares}"
+              f"\nDor de cabeça: {self.dor_cabeca}"
+              f"\nCoriza/congestão nasal: {self.coriza}")"""
