03ea236875b49d278157f3d9c990bc359eb3f030
clecio1201 <you@example.com>
2020-04-23 19:47:09 -0300

    Alteracao no menu de opcoes

--- projeto-corona-virus/teste.py
+++ projeto-corona-virus/teste.py
@@ -1,8 +1,12 @@
 from hospital import Hospital
+from time import sleep
+
+#tabela c/cores
+red = "\033[1;31m"
+green = "\033[0;32m"
+reset = "\033[0;0m"
 
 paciente = Hospital()
-
-
 
 while (True):
 
@@ -12,31 +16,36 @@ while (True):
           "\n\t\t3 - LISTAR PACIENTES GERAL"
           "\n\t\t4 - SAIR")
 
-
     opcao = int(input("\n\t\tDIGITE UMA OPCAO :"))
 
     if (opcao == 4):
+        print(green, "Saindo do sistema...", reset)
+        sleep(2)
         break
     else:
-        if(opcao == 1):
-            while(True):
+        if (opcao == 1):
+            while (True):
                 print("\n\t\tCADASTRO DO PACIENTE")
-                print("\t ","_" * 25)
+                print("\t ", "_" * 25)
                 paciente.cadastrar_pacientes()
                 paciente.diagnostico()
                 paciente.resultado()
+
                 print("\n N - NOVO PACIENTE"
-                             "\t\tS - SAIR")
+                      "\t\tS - SAIR")
                 sair = input("OPÇÃO:")
                 if sair == 's':
                     break
                 elif sair == 'n':
                     continue
 
-        elif(opcao == 2):
+        elif (opcao == 2):
             paciente.listar_pacientes_infectados()
 
-        elif(opcao == 3):
+        elif (opcao == 3):
             paciente.listar_pacientes_geral()
+
+        else:
+            print(red, "Digite uma opção válida", reset)
 
 
